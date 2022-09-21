fa92e0496c7ebb05437b9362351f06a1b28b4de4
skylarlynch <78100572+skylarlynch@users.noreply.github.com>
2022-09-21 16:33:19 -0600

    Add files via upload

---
+++ coding_assignment5.R
@@ -0,0 +1,51 @@
+# Adapting SSQ for paramecium data
+
+linmod <- function(b_0, b_1, x) {
+  y <- b_0 + b_1 * x
+  return(y)
+}
+
+
+ssq_linmod <- function(p, y, x) {
+  y_pred <- linmod(b_0=p[1], b_1=p[2], x) 
+  e <- y - y_pred 
+  ssq <- sum(e^2)
+  return(ssq)
+}
+
+
+
+paramecium <- read.csv("paramecium.csv")
+
+# Plot the data
+plot(paramecium$ï..t, paramecium$N)
+
+# My R did something weird with the header when I entered the csv, but it works if I use that header label
+
+# Train the model using the Nelder-Mead descent algorithm
+
+
+# Initialize parameters 
+b_0_start <- 200
+b_1_start <- -5
+
+# Finding the minimum SSQ
+
+
+par <- c(b_0_start, b_1_start)  
+fit <- optim(par, ssq_linmod, y=paramecium$N, x=paramecium$ï..t)
+fit
+
+
+# Calculate fitted model for best parameter values.
+
+y_pred <- linmod(b_0=fit$par[1], b_1=fit$par[2], paramecium$ï..t)
+
+# Plot fitted model with the data
+points(paramecium$ï..t, y_pred, col="red")
+abline(fit$par[1], fit$par[2], col="red")
+
+# lm() for comparison.
+lmfit <- lm(N ~ ï..t, data=paramecium)
+lmfit 
+anova(lmfit) 
